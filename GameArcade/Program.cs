a208f921c21229a24491083ed6a93c3fcef12af3
lukassei <lukas.sei@seznam.cz>
2018-01-18 18:48:07 +0100

    Try of a commit change.

---
+++ GameArcade/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameArcade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello GitHub.";
+            Console.WriteLine(s);
+            Console.ReadKey();
+            //try
+        }
+    }
+}
